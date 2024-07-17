5a6e9d24d4a2773a9eb8dd41552916f762613466
casb62 <casb62@gmail.com>
2024-07-17 12:02:48 -0300

    Projeto criado.

---
+++ src/main/java/com/casb/enchentesBrasil/EnchentesBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.casb.enchentesBrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchentesBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnchentesBrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casb/enchentesBrasil/EnchentesBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casb.enchentesBrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnchentesBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
